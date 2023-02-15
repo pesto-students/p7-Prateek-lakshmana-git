c582f4d6b46d088f1f4a23dd636cc40e659aa828
lakshmana-git <devlakshmanarao@gmail.com>
2023-02-15 22:33:31 +0530

    shorlty live

--- Week-10/shortly-app/src/App.js
+++ Week-10/shortly-app/src/App.js
@@ -1,12 +1,38 @@
-
+import axios from "axios";
+import React, { useState } from "react";
+import {Home} from './Home';
 import './App.css';
-
+import { Header } from './Header';
+import {Footer} from './Footer';
 function App() {
-  return (
-    <header className="header-main">
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+  const [url,setUrl] =useState([])
+  const fetchData = async () => {
+    try {
+      const res = await axios(`https://api.shrtco.de/v2/shorten?url=${input}`);
+      setResult(res.data.result.full_short_link)
+      setUrl((prevUrls)=>[...prevUrls,input])
+      ;
+    } catch (err) {
+      alert(err);
+    }
+  };
+  console.log(url)
 
-      
-    </header>
+  const handler = () => {
+    fetchData();
+    setInput("");
+    console.log(input);
+  };
+  return ( <>
+    <Header/>
+    <Home handler={handler} input={input} setInput={setInput} result={result} url={url}/>
+    <Footer/>
+    
+    
+    
+  </>
   );
 }
 
